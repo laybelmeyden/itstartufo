c8a80d2a1c3e7c5f583825218517414cc3504b1b
laybelmeyden <trycollens@gmail.com>
2020-10-08 16:51:40 +0300

    fixed/news

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -259,8 +259,8 @@
 						@foreach ($thenews as $thenew)
 						<div class="col-lg-4 col-md-4 blog-left">
 							<a href="/thenewsolo{{ $thenew->id }}">
-							<div class="thumb img_overf">
-								<img class="img-fluid" src="storage/{{ $thenew->new_img }}" alt="">
+							<div class="thumb img_overf" style="background-image: url(storage/{{ $thenew->new_img }});">
+								<!-- <img class="img-fluid" src="storage/{{ $thenew->new_img }}" alt=""> -->
 							</div>
 							<div class="detais">
 								<ul class="tags">
